ef54e29e884c4dc63ceb17babc91c770738e9fb8
Preet2099x <preet2099x@gmail.com>
2025-04-11 18:45:48 +0530

    added active projects in citizen dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Dashboard from './pages/CitizenDashboard';
 import Signup from './pages/SignUp';
+import CitizenDashboard from './pages/CitizenDashboard';
+import ActiveProjects from './pages/Citizen/ActiveProjects';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/signup" element={<Signup />} />
 
+      <Route path="/dashboard" element={<CitizenDashboard />}>
+        <Route index element={<ActiveProjects />} />
+        {/* Other nested routes can go here */}
+      </Route>
     </Routes>
   );
 };
--- client/src/pages/CitizenDashboard.jsx
+++ client/src/pages/CitizenDashboard.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import CitizenSidebar from "../components/CitizenSidebar";
+import { Outlet } from "react-router-dom";
 
-const CitizenDashboard = ({ children }) => {
+const CitizenDashboard = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Fixed Navbar */}
       <Navbar />
-
-      {/* Main Content Area */}
       <div className="flex pt-16">
-        {/* Sidebar Container */}
         <div className="w-72 h-[calc(100vh-4rem)] sticky top-16 overflow-y-auto border-r border-gray-200">
           <CitizenSidebar />
         </div>
-
-        {/* Main Content */}
-        <main className="flex-1 p-8">{children}</main>
+        <main className="flex-1 p-8">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
