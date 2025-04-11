a8409306b5c3f8a505aca4664f0c05fd2556cc45
Preet2099x <preet2099x@gmail.com>
2025-04-11 17:14:28 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,28 @@
-import "./App.css";
-import CitizenDashboard from "./pages/CitizenDashboard";
-// import PartyDashboard from "./pages/PartyDashboard";
-// import GovernanceDashboard from "./pages/GovernanceDashboard";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CitizenDashboard from './pages/CitizenDashboard'
+import GovernanceDashboard from './pages/GovernanceDashboard'
+import PartyDashboard from './pages/PartyDashboard'
+import ActiveProjects from './pages/Citizen/ActiveProjects'
+
 function App() {
   return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<CitizenDashboard />}>
+    //       <Route index element={<ActiveProjects />} />
+    //       <Route path="active-projects" element={<ActiveProjects />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
     <>
-      <CitizenDashboard />
-      {/* <PartyDashboard /> */}
-      {/* <GovernanceDashboard /> */}
+    <CitizenDashboard/>
+    <PartyDashboard/>
+    <GovernanceDashboard/>
+    <ActiveProjects/>
+
     </>
-  );
+ 
+  )
 }
 
-export default App;
+export default App
