5f9ccdf1f5d951ff971508bbbc1330d7fc22bf8c
Preet2099x <preet2099x@gmail.com>
2025-04-11 16:17:18 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 // import CitizenDashboard from "./pages/CitizenDashboard";
 // import PartyDashboard from "./pages/PartyDashboard";
 // import GovernanceDashboard from "./pages/GovernanceDashboard";
-import Signup from "./pages/Signup";
 function App() {
   return (
     <>
