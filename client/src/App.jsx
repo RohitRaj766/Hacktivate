cfc682b241524827dc207fdb02bc7a88f1c02529
Chandanmajhi7 <chandanmajhi9693@gmail.com>
2025-04-11 16:08:45 +0530

    publishing a branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-// import CitizenDashboard from "./pages/CitizenDashboard";
-// import PartyDashboard from "./pages/PartyDashboard";
-// import GovernanceDashboard from "./pages/GovernanceDashboard";
+import CitizenDashboard from "./pages/CitizenDashboard";
+import PartyDashboard from "./pages/PartyDashboard";
+import GovernanceDashboard from "./pages/GovernanceDashboard";
 import Signup from "./pages/Signup";
 function App() {
   return (
     <>
-      {/* <CitizenDashboard /> */}
-      {/* <PartyDashboard /> */}
-      {/* <GovernanceDashboard /> */}
+      {<CitizenDashboard />}
+      {<PartyDashboard />}
+      {<GovernanceDashboard />}
       <Signup />
     </>
   );
